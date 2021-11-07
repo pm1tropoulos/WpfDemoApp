40dad205806822d23b89373401d6c696e6dd1d6f
Panagiotis Mitropoulos <pmitropoulos@outlook.com.gr>
2021-11-07 22:28:16 +0200

    solution readded.
    Removed solution files readded..

---
+++ DL/DL.Customer/Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DL.Customer
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+
+    }
+}
---
+++ UI/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("Button clicked!!");
+        }
+    }
+}
+
