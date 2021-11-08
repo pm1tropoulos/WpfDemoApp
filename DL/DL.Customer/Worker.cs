9b3528ce2560f0d6efeb9fdd8de8b1bd3b849b06
Panagiotis Mitropoulos <pmitropoulos@outlook.com.gr>
2021-11-08 21:21:11 +0200

    worker added

---
+++ DL/DL.Customer/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DL.Customer
+{
+
+    public delegate int WorkPerformedHandler(int hours, WorkType workType);
+    public class Worker
+    {
+        public event WorkPerformedHandler WorkPerformed;
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+
+        }
+
+    }
+
+    public enum WorkType
+    {
+        GenerateReport,
+        Logistics
+    }
+}
