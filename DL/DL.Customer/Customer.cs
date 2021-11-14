d60245f5a43d522cd5706e634146d960a1c679db
Panagiotis Mitropoulos <pmitropoulos@outlook.com.gr>
2021-11-14 13:28:03 +0200

    Customer class fix

--- DL/DL.Customer/Customer.cs
+++ DL/DL.Customer/Customer.cs
@@ -4,7 +4,26 @@ namespace DL.Customer
 {
     public class Customer
     {
-        public string FirstName { get; set; }
+        //private readonly string _firstName;
+        //private readonly string _lastName;
+        //private readonly string _email;
+        //private readonly DateTime _birthDate;
+        //private readonly int _ID;
+
+        //public Customer(string firstName, string lastName, string email,
+        //    DateTime birthDate, int ID)
+        //{
+        //    _firstName = firstName;
+        //    _lastName = lastName;
+        //    _email = email;
+        //    _birthDate = birthDate;
+        //    _ID = ID;
+        //}
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+        public static DateTime BirthDate { get; set; }
+        public static string email { get; set; }
+        public static int ID { get; set; }
 
     }
 }
